55dd3ad12d9c1237ee2432a86ed6f22695ef60d3
Alissa Renz <alissa.renz@gmail.com>
2022-08-31 15:54:36 -0700

    Initial sample submission (#1)

--- manifest.ts
+++ manifest.ts
@@ -1,13 +1,13 @@
-import {
-  DefineFunction,
-  DefineWorkflow,
-  Manifest,
-  Schema,
-} from "deno-slack-sdk/mod.ts";
+import { Manifest } from "deno-slack-sdk/mod.ts";
 
+/**
+ * The app manifest contains the app's configuration. This
+ * file defines attributes like app name and description.
+ * https://api.slack.com/future/manifest
+ */
 export default Manifest({
-  name: "starter-template",
-  description: "A starter template.",
+  name: "deno-blank-template",
+  description: "A blank template for building Slack apps with Deno",
   icon: "assets/icon.png",
   functions: [],
   workflows: [],
