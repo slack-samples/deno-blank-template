6fe5dcdecbd8bd58e7bde49092fae33661487eaa
William Bergamin <wbergamin@slack-corp.com>
2022-11-01 14:37:09 -0400

    Change default Slack App Icon

--- manifest.ts
+++ manifest.ts
@@ -8,7 +8,7 @@ import { Manifest } from "deno-slack-sdk/mod.ts";
 export default Manifest({
   name: "deno-blank-template",
   description: "A blank template for building Slack apps with Deno",
-  icon: "assets/icon.png",
+  icon: "assets/default_new_app_icon.png",
   functions: [],
   workflows: [],
   outgoingDomains: [],
